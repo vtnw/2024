ab726d543f54834ea053d0870ff1a3457258c071
vtnw <vtnw@users.noreply.github.com>
2024-03-30 12:26:32 +0530

    Create common.js

---
+++ scripts/common.js
@@ -0,0 +1,61 @@
+<script>
+  var command = "";
+
+    window.addEventListener("load", () => loadNotes());
+
+    document.addEventListener("click", (event) => document.getElementById("tb").focus());
+
+    document.getElementById("tb").addEventListener("blur", (event) => processData(document.getElementById("tb").innerText));
+
+    document.getElementById("fl").addEventListener("change", () => importFile());
+
+    saveAsFile = (data, fileName) => {
+        let a = document.createElement("a");
+        a.download = fileName;
+        a.innerHTML = "export";
+        a.href = window.URL.createObjectURL(new Blob([data], { type: "text/plain" }));
+        a.style.display = "none";
+        a.onclick = function (event) { document.body.removeChild(event.target); };
+        document.body.appendChild(a);
+        a.click();
+    }
+
+    processData = (text) => {
+        if (!text || /\s$/.test(text)) {
+            return;
+        }
+
+        if (!text.startsWith(".")) {
+            text = `.add ${text}`;
+        }
+
+        document.getElementById("tb").innerText = "";
+        let all = text.split(" ");
+        let action = all[0];
+        let content = text.substring(action.length + 1);
+
+        switch (action) {
+            case "..": {
+                document.getElementById("tb").innerText = command;
+                break;
+            }
+            default: {
+                processNotes(action, content);
+              break;
+            }
+        }
+
+        command = text;
+    }
+
+    doClear = () => { showData(""); showStatus("done", true); }
+
+    showData = (text) => document.getElementById("dv").innerText = text;
+
+    showStatus = (text, append) => document.getElementById("spn").innerText = `${append ? document.getElementById("spn").innerText + " - " : ""}${text}`;
+
+    isoToString = (date) => {
+        date = new Date(date);
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+</script>
