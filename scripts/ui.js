65f8b9ea2c5fb1b54f286917d64b0e21ec57424c
vtnw <vtnw@users.noreply.github.com>
2024-05-09 11:56:46 +0530

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -281,7 +281,7 @@ bindDates = () => {
         dates.find(y => y.name == year).months.find(m => m.name == month).weeks.find(w => w.name == week).days.forEach(d => {
             addDiv(dvDates, d, null, "date", (event) => {
                 day = event.target.innerText;
-                selectedDates.push(isoToString(new Date(`${day.split(" ")[0]}-${month}-${year}`)))
+                selectedDates.push(isoToString(new Date(`${parseInt(day)}-${month}-${year}`)))
                 bindDates();
                 bindFilters();
             });
@@ -337,14 +337,14 @@ initializeDates = () => {
             let w = 1;
             for (let d = new Date(y, m, 1); d <= new Date(y, m + 1, 0); d.setDate(d.getDate() + 1)) {
                 days.push(d.getDate());
-                weekdays.push(`${d.getDate().toString().padStart(2,"0")} ${d.toLocaleString("default", { weekday: "short" }).toLowerCase().slice(0, -1)}`);
+                weekdays.push(`${d.getDate().toString().padStart(2,"0")}  ${d.toLocaleString("default", { weekday: "short" }).toLowerCase().slice(0, -1)}`);
                 if (d.getDay() == 0 || d.getDate() ==  new Date(y, m + 1, 0).getDate()) {
                     let currWeek = {name: `${days[0]}-${days.at(-1)}`, days: weekdays};
                     currMonth.weeks.push(currWeek);
                     if (y == date.getFullYear() && m == date.getMonth() && days.includes(date.getDate())) {
                         week = currWeek.name;
                         let currDayIndex = days.indexOf(date.getDate());
-                        currWeek.days[currDayIndex] = `${currWeek.days[currDayIndex]}°`;
+                        currWeek.days[currDayIndex] = `${currWeek.days[currDayIndex]}.replace("  ", "° ")`;
                     }
                     w++;
                     days = [];
